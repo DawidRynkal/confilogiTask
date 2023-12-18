d52498a36954e0351c7d20ed8ba5a6395d98576f
Dawid Rynkal <rynkal.dawid@gmail.com>
2023-12-18 13:53:15 +0100

    New branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to refffload.
+          Edit <code>src/App.tsx</code> and save to rd.
         </p>
         <a
           className="App-link"
